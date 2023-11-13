08501d4cb042b933f56d3091f3ce4ce924054665
M. César Brancalhão <58019405+cesarbrancalhao@users.noreply.github.com>
2023-11-13 20:49:30 -0300

    Create BookController.cs

---
+++ Controller/BookController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace Bookshelf
+{
+	public class BookController : Controller
+	{
+		[HttpGet]
+		public ActionResult Index()
+		{
+			return View(new Book());
+		}
+
+
+		[HttpPost]
+		public JsonResult GetBook(string BookName, int BookNumPages, string ISBN)
+		{				
+			//int index = _rnd.Next(_db.Count);
+			//var answer = _db[index];
+			return Json('a');
+		}
+		
+		private static List<string> _listStrings = new List<string>
+		{ 
+			"Works"
+		};
+	}
+}
