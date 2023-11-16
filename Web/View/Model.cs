59b1b4b9e56770c49bce240c8037d108c8a586ba
M. César Brancalhão <58019405+cesarbrancalhao@users.noreply.github.com>
2023-11-16 20:29:36 -0300

    Create Model.cs

---
+++ Web/View/Model.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookshelf
+{
+	public class Book
+	{
+		[Required]
+		[MinLength(10)]
+		[MaxLength(100)]
+		[Display(Name = "Book name:")]
+		public string BookName { get; set; }
+
+		[Required]
+		[Display(Name = "Number of pages:")]
+		public int BookNumPages { get; set; }
+		
+		[Display(Name = "ISBN:")]
+		public string ISBN { get; set; }
+	}
+}
